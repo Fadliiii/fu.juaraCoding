c2ba5cedc5466d8fcb9a64311d1a2c42cd7dd58e
fadli <fadlilatul.umamm@gmail.com>
2024-12-02 20:34:06 +0700

    s

---
+++ src/main/java/com/juaraCoding/javaFundamental/Main.java
@@ -0,0 +1,8 @@
+package com.juaraCoding.javaFundamental;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("umam");
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/juaraCoding/javaFundamental/contoh.java
@@ -0,0 +1,7 @@
+package com.juaraCoding.javaFundamental;
+
+public class contoh {
+    public static void main(String[] args) {
+        System.out.println("hallo");
+    }
+}
